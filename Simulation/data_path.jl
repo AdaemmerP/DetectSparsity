eaef0368c5f977763da1d85f1b49b851dac55ff0
Philipp Adämmer <adaemmerp@uni-greifswald.de>
2023-10-18 15:15:35 +0200

    prepare server

--- Simulation/data_path.jl
+++ Simulation/data_path.jl
@@ -1,2 +1,2 @@
 data_path = "/home/adaemmerp/Dropbox/HSU/Projekte/Mit_Rainer/GLP_SparseDense/Codes_GH/DetectSparsity_Julia_1.9/Data/"
-#data_path = "~/Data/" # for server
+data_path = "~/DetectSparsity/Data/" # for server
